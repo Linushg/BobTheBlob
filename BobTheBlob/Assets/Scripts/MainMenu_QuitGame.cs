3355273810e801d1096a2a7fcf03bb7febff8ff5
anre1774 <anre1774@student.su.se>
2021-04-30 16:14:08 +0200

    Menu Added
    -Temporary menu added

---
+++ BobTheBlob/Assets/Scripts/MainMenu_QuitGame.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenu_QuitGame : MonoBehaviour
+{
+
+    public void QuitGame() {
+        Application.Quit();
+
+    }
+}
---
+++ BobTheBlob/Assets/Scripts/MainMenu_StartGame.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu_StartGame : MonoBehaviour
+{
+  
+   public void StartGame() {
+        PlayerPrefs.SetInt("CoinAmount", 0);
+        SceneManager.LoadScene(1);
+
+
+    }    
+    
+}
