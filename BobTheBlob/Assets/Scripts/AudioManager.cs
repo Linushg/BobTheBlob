c17462be967841b5cf99c0ebfa64a4e5d12b33f5
vanjaodmann <71493665+vanjaodmann@users.noreply.github.com>
2021-05-28 14:15:09 +0200

    Ljudscript fix

--- BobTheBlob/Assets/Scripts/AudioManager.cs
+++ BobTheBlob/Assets/Scripts/AudioManager.cs
@@ -9,7 +9,11 @@ public class AudioManager : MonoBehaviour {
     void Awake() {
         foreach (Sound s in sounds)
         {
-            gameObject.AddComponent<AudioSource>();
+            s.source = gameObject.AddComponent<AudioSource>();
+            s.source.clip = s.clip;
+
+            s.source.volume = s.volume;
+            s.source.pitch = s.pitch;
         }
     }
 
--- BobTheBlob/Assets/Scripts/Sound.cs
+++ BobTheBlob/Assets/Scripts/Sound.cs
@@ -9,7 +9,7 @@ public class Sound {
     public AudioClip clip;
 
     [Range(0f, 1f)]
-    public float volyume;
+    public float volume;
     [Range(.1f, 3f)]
     public float pitch;
 
