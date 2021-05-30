c7c45a1faed5fe8ae355951e304ca749c7484c81
Jossanl <71289092+Jossanl@users.noreply.github.com>
2021-05-30 18:31:16 +0200

    Kommentarer på script

--- BobTheBlob/Assets/Scripts/PlayerState.cs
+++ BobTheBlob/Assets/Scripts/PlayerState.cs
@@ -5,66 +5,51 @@ using UnityEngine.SceneManagement;
 
 public class PlayerState : MonoBehaviour
 {
-  // Bob the blob
-    public int healthPoints = 2;
-    public int initialHealthPoints = 3;
-    public int ingredientAmount = 0;
+    public int healthPoints = 2; // Värdet av HP
+    public int initialHealthPoints = 3; // Så många HP Bob börjar med
+    public int ingredientAmount = 0; // Urspringliga värdet av Ingrediensen
 
-    public GameObject respawnPosition;
-    public GameObject startPosition;
-    [SerializeField] public bool useStartPosition;
-    [SerializeField] int gameover;
+    public GameObject respawnPosition; // initierad plats för Respawn
+    public GameObject startPosition; // initierad plats för Start-positionen
+    [SerializeField] public bool useStartPosition; //För att kunna ändra utanför scriptet i inspektorn för använda startpositionen
+    [SerializeField] int gameover; //För att kunna ändra i inspektorn för ändra GameOver
   
-
-    // Start is called before the first frame update
     void Start()
     {
-        healthPoints = initialHealthPoints;
-        startPosition = GameObject.Find("start");
+        healthPoints = initialHealthPoints; // Börjar spelet med antalet HP man har
+        startPosition = GameObject.Find("start"); //För start 
         if (useStartPosition == true){
             gameObject.transform.position = startPosition.transform.position;
             
         }
-        respawnPosition = startPosition; 
+        respawnPosition = startPosition; // Respawnpositionen blir startpositionen
     }
 
-    // Update is called once per frame
-   /* void Update() //kanske jättekonstigt
-    {
-        private void onTriggerEnter2D(Collider collision){
-            if (collision.CompareTag("Wall")== true){
-               Shake(this); //gameobject
-            }
-        }
-
-    }*/
-
-    public void DoHarm(int doHarmByThisMuch) {
+    public void DoHarm(int doHarmByThisMuch) { // För att Player ska ta skada
         healthPoints -= doHarmByThisMuch;
-        if (healthPoints <= 0) 
+        if (healthPoints <= 0) // När Player inte har HP kvar ska Bob dö
         {
-            SceneManager.LoadScene(gameover);
+            SceneManager.LoadScene(gameover); // Då ska en GameOver Scen visas
         }
         else
         {
-            gameObject.transform.position = startPosition.transform.position;
-            
+            gameObject.transform.position = startPosition.transform.position; // Om det inte är 0 så ska Player börja på startpositionen
         }
 
     }
 
-    public void Respawn()
+    public void Respawn() // För att respawna
     {
-        healthPoints = initialHealthPoints;
+        healthPoints = initialHealthPoints; // Player börjar med de initierard HP
     }
 
-    public void IngredientPickup()
+    public void IngredientPickup() // Pickup för ingredienser, då kommer det ökas och visas via UI
     {
-        ingredientAmount++;
+        ingredientAmount++; // Det ökas
     }
 
-    public void ChangeRespawnPosition(GameObject newRespawnPosition) {
-        respawnPosition = newRespawnPosition;
+    public void ChangeRespawnPosition(GameObject newRespawnPosition) { // Är till för att byta respawn position
+        respawnPosition = newRespawnPosition; // Initierar en ny position
     }
 
 }
